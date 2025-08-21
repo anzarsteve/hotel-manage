19c8bda2d0fa53423f65171786e293f5414dabfb
Antonio Salazar <antonio.salazar@et.bo>
2025-08-21 18:11:07 -0400

    subida del proyecto

---
+++ src/test/java/bo/anzar/hotel_manage/HotelManageApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.anzar.hotel_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
